cd74774f06f648eac0b1198f3ae355c27db65891
christian <christian96.wo@gmail.com>
2014-03-11 14:34:58 +0100

    BoardView draws a simple 8 x 6 field

--- src/com/example/isola/BoardView.java
+++ src/com/example/isola/BoardView.java
@@ -5,16 +5,22 @@ import java.util.Observer;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 public class BoardView extends View implements Observer {
-	
+	private boolean initialized = false;
 	private Board board;
+	private int height_display;
+	private int width_display;
+	private int side_length_square;
 	
 	public BoardView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -24,11 +30,40 @@ public class BoardView extends View implements Observer {
 		super(context);
 		this.board = board;
 		invalidate();
+		
+		//getting the screen resolution
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		Display display = wm.getDefaultDisplay();
+		//the new solution would require at least API level 13 (current min is 8)
+		height_display = display.getHeight();
+		width_display = display.getWidth();
 	}
 	
-	@Override
+	
+	private void initiate_drawing(Canvas canvas) {
+		Bitmap tyle_free = BitmapFactory.decodeResource(getResources(), R.drawable.tyle_free);
+		side_length_square = (int) width_display / 8;
+		Bitmap tyle_free_resized = Bitmap.createScaledBitmap(tyle_free, side_length_square, side_length_square, true);
+		int pos_x = side_length_square;
+		int pos_y = (int) (height_display - width_display) / 2; //width_display = 8 * side_length_square
+		for(int i = 1; i <= 8; i++) {
+			pos_x = side_length_square;
+			for(int k = 1; k <= 6; k++) {
+				canvas.drawBitmap(tyle_free_resized, pos_x, pos_y, null);
+				pos_x += side_length_square;
+			}
+			pos_y += side_length_square;
+		}
+		
+		initialized = true;
+	}
+	
 	protected void onDraw(Canvas canvas) {
-		canvas.drawColor(Color.MAGENTA);
+		if(!initialized) {
+			initiate_drawing(canvas);
+		} else {
+			// TODO: destroy tyle or move player
+		}
 	}
 
 	@Override
