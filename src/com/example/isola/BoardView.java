531f6abec61a845a4e2a24bab9d96cb4e1311736
David Südholt <david.suedholt@gmail.com>
2014-03-11 14:59:01 +0100

    Changed BoardView implemenation

--- src/com/example/isola/BoardView.java
+++ src/com/example/isola/BoardView.java
@@ -4,23 +4,22 @@ import java.util.Observable;
 import java.util.Observer;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
 
 public class BoardView extends View implements Observer {
-	private boolean initialized = false;
+	
 	private Board board;
-	private int height_display;
-	private int width_display;
-	private int side_length_square;
+	private Paint paint;
+	private Rect rect;
+	
+	private int tilewidth, tileheight;
 	
 	public BoardView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -29,50 +28,50 @@ public class BoardView extends View implements Observer {
 	public BoardView(Context context, Board board) {
 		super(context);
 		this.board = board;
+		paint = new Paint();
+		paint.setColor(Color.BLACK);
+		paint.setStrokeWidth(3);
 		invalidate();
-		
-		//getting the screen resolution
-		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-		Display display = wm.getDefaultDisplay();
-		//the new solution would require at least API level 13 (current min is 8)
-		height_display = display.getHeight();
-		width_display = display.getWidth();
+		rect = new Rect();
 	}
 	
-	
-	private void initiate_drawing(Canvas canvas) {
-		Bitmap tyle_free = BitmapFactory.decodeResource(getResources(), R.drawable.tyle_free);
-		side_length_square = (int) width_display / 8;
-		Bitmap tyle_free_resized = Bitmap.createScaledBitmap(tyle_free, side_length_square, side_length_square, true);
-		int pos_x = side_length_square;
-		int pos_y = (int) (height_display - width_display) / 2; //width_display = 8 * side_length_square
-		for(int i = 1; i <= 8; i++) {
-			pos_x = side_length_square;
-			for(int k = 1; k <= 6; k++) {
-				canvas.drawBitmap(tyle_free_resized, pos_x, pos_y, null);
-				pos_x += side_length_square;
-			}
-			pos_y += side_length_square;
-		}
-		
-		initialized = true;
+	public Point getTileCoords(int x, int y) {
+		tilewidth = getWidth() / Board.WIDTH;
+		tileheight = getHeight() / Board.HEIGHT;
+		return new Point(x / tilewidth, y / tileheight);
 	}
 	
+	@Override
 	protected void onDraw(Canvas canvas) {
-		if(!initialized) {
-			initiate_drawing(canvas);
-		} else {
-			// TODO: destroy tyle or move player
+		tilewidth = getWidth() / Board.WIDTH;
+		tileheight = getHeight() / Board.HEIGHT;
+		canvas.drawColor(Color.WHITE);
+		for (int i = 0; i < Board.WIDTH; i++) {
+			for (int j = 0; j < Board.HEIGHT; j++) {
+
+				rect.set(i * tilewidth, j * tileheight, (i + 1) * tilewidth, (j + 1) * tileheight);
+				switch(board.getTile(i, j)) {
+				case PLAYER1:
+					paint.setColor(Color.RED); break;
+				case PLAYER2:
+					paint.setColor(Color.BLUE); break;
+				case FREE:
+					paint.setColor(Color.BLACK); break;
+				case DESTROYED:
+					paint.setColor(Color.BLACK); break;
+				}
+				if (board.getTile(i, j) == Tile.FREE) {
+					paint.setStyle(Style.STROKE);
+				} else {
+					paint.setStyle(Style.FILL);
+				}
+				canvas.drawRect(rect, paint);
+			}
 		}
 	}
 
 	@Override
 	public void update(Observable board, Object o) {
 		invalidate();
-	}
-	
-	protected Point getTileCoords(int x, int y) {
-		Point placeholder = new Point(1, 1);
-		return placeholder;
 	}
 }
