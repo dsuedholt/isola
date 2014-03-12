d94948bdad32ee9a47149a033b788acaef2a8551
christiank11 <christian96.wo@gmail.com>
2014-03-12 08:48:14 +0100

    InfoActivity added

--- src/com/example/isola/MainActivity.java
+++ src/com/example/isola/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -54,6 +55,11 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		System.out.println("Eintrag erkannt");
 		return true;
 	}
 
