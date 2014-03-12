aebfe3bac36c728026d127b8872c7e06420ebc33
christiank11 <christian96.wo@gmail.com>
2014-03-12 09:13:58 +0100

    made textview in activity_help scrollable

--- src/com/example/isola/HelpActivity.java
+++ src/com/example/isola/HelpActivity.java
@@ -3,10 +3,13 @@ package com.example.isola;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HelpActivity extends Activity {
 
@@ -15,12 +18,17 @@ public class HelpActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
 		
+		TextView how_to_play = (TextView) findViewById(R.id.text_view_how_to_play);
+		how_to_play.setMovementMethod(new ScrollingMovementMethod());
+		
 		Button close_activity = (Button) findViewById(R.id.button_close_help);
+		
 		close_activity.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				finish();
 			}
 		});
+		
 	}
 
 }
